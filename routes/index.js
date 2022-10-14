b68636bd6f0b5fd879548a4c8af5548495b9f6ef
CharlesBeatty <charles.beatty@tcg.com>
2022-10-14 19:43:19 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var envName = 'ENV: ' + process.env.COPILOT_ENVIRONMENT_NAME;
+var envName = process.env.COPILOT_ENVIRONMENT_NAME;
+if (envName === undefined) {
+    envName = 'Local';
+}
+var title = 'ENV: ' + envName;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.render('index', { title: envName });
+  res.render('index', { title: title });
 });
 
 module.exports = router;
