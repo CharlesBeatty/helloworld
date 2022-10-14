90552b3d6aed74a6e8e1be4367221ef3f7155fca
CharlesBeatty <charles.beatty@tcg.com>
2022-10-14 19:30:19 -0400

    Fresh start

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var envName = 'ENV: ' + process.env.COPILOT_ENVIRONMENT_NAME;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.render('index', { title: envName });
 });
 
 module.exports = router;
