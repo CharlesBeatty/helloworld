5f336a7ab5f491b96255e729fd5838800c3b6a9f
CharlesBeatty <charles.beatty@tcg.com>
2022-10-25 12:48:51 -0400

    Redo from START

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   database: 'my_db',
   password: 'password',
   port: 5432,
-  host: 'postgres.test.greetings.local'   // from AWS VPC network
+  host: 'database.test.helloworld.local'   // from AWS VPC network
   //host: '172.17.0.2'   // from Docker network
 })
 
